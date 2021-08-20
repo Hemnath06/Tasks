cbad16c91a87daa84d3a41c20bdf5293cb74014b
Hemnath06 <61081937+Hemnath06@users.noreply.github.com>
2021-08-20 09:51:08 +0530

    commit 5

---
+++ Task-26(React-Saga(CRUD))/src/Redux-Saga3/RootSaga.js
@@ -0,0 +1,25 @@
+import { all } from "@redux-saga/core/effects";
+import { watchGetUsers,
+
+  watchCancelUser,
+
+  watchDeleteUser,
+  watchPostUser,
+
+  watchPutUser,
+
+  watchEditUser } from "./Saga3";
+export default function* rootsaga() {
+  yield all([
+    watchGetUsers(),
+
+    watchCancelUser(),
+
+    watchDeleteUser(),
+    watchPostUser(),
+
+    watchPutUser(),
+
+    watchEditUser(),
+  ]);
+}
